f7cacbcdb69c6fba96f1026f5c2a3aed6ccaa379
sagar <sagarsrivastava347@gmail.com>
2026-03-15 12:57:10 +0530

    changes in the all file

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ NOTHING
 this is one in the world 
 # vishu 
 this is sagar 
+# sagar 
+this is only one 
+# use the coding in the program 
+html css js redmi file 
---
+++ index.js
@@ -0,0 +1,14 @@
+let color = "Red"
+if (color== "green"){
+    console.log ("go")
+
+}
+else if (color== "red"){
+    console.log ("stop")
+}
+else if (color=="yellow"){
+    console.log ("slow")
+}
+else{
+    console.log ("bhad me jao")
+}
